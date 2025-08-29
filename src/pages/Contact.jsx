058b9619c72183f35d9cf3c571667cdfff57b2f9
Ruth Dorcas <106535441+RuthAdora@users.noreply.github.com>
2025-08-29 21:16:27 +0300

    vercel

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,33 +1,87 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+
+    emailjs
+      .send(
+        "service_oe1avbw",
+        "template_t8hbe86",
+        formData,
+        "SmPsNSZlQ-CaYxFLy"
+      )
+      .then(
+        () => {
+          setStatus("Message sent successfully!");
+          setFormData({ name: "", email: "", message: "" });
+        },
+        (error) => {
+          console.error(error);
+          setStatus("Failed to send message.");
+        }
+      );
+  };
+
   return (
     <section className="max-w-6xl mx-auto px-4 py-16">
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-6">
         Contact Us
       </h2>
-      <form className="max-w-2xl mx-auto bg-white shadow-lg rounded-xl p-8 space-y-4">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-2xl mx-auto bg-white shadow-lg rounded-xl p-8 space-y-4"
+      >
         <input
           type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
           placeholder="Your Name: "
           className="w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          required
         />
         <input
           type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
           placeholder="Your Email: "
           className="w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          required
         />
         <textarea
           rows="5"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
           placeholder="Your Message: "
           className="w-full border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          required
         ></textarea>
         <button
           type="submit"
-          className="w-full bg-indigo-600  py-2 rounded-lg hover:bg-indigo-700 transition"
+          className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition"
         >
           Send Message
         </button>
+        {status && <p className="text-center mt-2 text-sm">{status}</p>}
       </form>
     </section>
   );
 }
+
 export default Contact;
